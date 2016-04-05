a0c20e734681660fe27416be52a55b1a1cafb927
Tommy Li <h403l224@ku.edu>
2016-04-04 22:56:02 -0500

    Initial Commit

---
+++ One-D Cyclic Partition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  One-D Cyclic Partition
+//
+//  Created by 雪竜 on 16/4/4.
+//  Copyright © 2016年 雪竜. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
